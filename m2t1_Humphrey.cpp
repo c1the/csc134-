9d6c0c4eb36cee0d89338b301929a1bd52fb3d74
c1the <humphreygabrielle771@gmail.com>
2025-02-04 21:02:47 +0000

    modified:   m2t1_Humphrey 	modified:   m2t1_Humphrey.cpp

--- m2t1_Humphrey.cpp
+++ m2t1_Humphrey.cpp
@@ -7,6 +7,20 @@
 using namespace std;
 
 int main(){
-    cout << "Hello" << endl;
+    string name = "Gabrielle";
+    int num_apples = 10;
+    double price_each = 0.25;
+
+     
+    cout << "Welcome to the " << name << " apple farm" << endl;
+    cout << "We have " << num_apples << " in stock" << endl;
+    cout << "Each apple costs " << price_each << endl;
+
+    
+    double total_price = num_apples * price_each;
+
+    cout << "The total price for all " << num_apples;
+    cout << " is: $" << total_price << endl;
+    cout << "Thanks for coming by!" << endl;
     return 0;
 }
