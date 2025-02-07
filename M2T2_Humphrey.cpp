7ae587723ad1879c5cc4ef3a5d7cc7e382784b21
c1the <humphreygabrielle771@gmail.com>
2025-02-07 17:02:13 -0500

    Update M2T2_Humphrey.cpp

--- M2T2_Humphrey.cpp
+++ M2T2_Humphrey.cpp
@@ -1,1 +1,29 @@
+// CSC 134
+// M2T2
+// Gabrielle Humphrey
+// 2/7/2025
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int num_apples;
+    double price_each;
+
+    cout << "What is your name? " << endl;
+    cin >> name;
+
+     
+    cout << "Welcome to  " << name << "'s apple store" << endl;
+    cout << "We have " << num_apples << " in stock" << endl;
+    cout << "Each apple costs " << price_each << endl;
+
+    
+    double total_price = num_apples * price_each;
+
+    cout << "The total price for all " << num_apples;
+    cout << " is: $" << total_price << endl;
+    cout << "Thanks for coming by!" << endl;
+    return 0;
+}
