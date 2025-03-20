326bfab9db3b40b3e39e460e55e9bd4a03a9da6c
c1the <humphreygabrielle771@gmail.com>
2025-03-20 14:05:00 +0000

    modified:   .vscode/settings.json 	new file:   M4T1_Humphrey 	new file:   M4T1_Humphrey.cpp

---
+++ M4T1_Humphrey.cpp
@@ -0,0 +1,29 @@
+/*
+CSC 134
+M4T1
+Gabrielle Humphrey
+3/20/2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Program 5-3
+    int number = 0;
+    while (number < 5)
+    {
+        cout << "Hello\n";
+        number++;
+    }
+    cout << "That's all!\n";
+
+    //Program 5-6
+    const int MIN_NUMBER = 1, MAX_NUMBER = 10;
+
+    int num = MIN_NUMBER;
+    cout << "Number Number Squared\n";
+    cout << "-------------------------\n";
+    return 0;
+}
